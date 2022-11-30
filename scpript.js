64361b24db8c24b89592833129f9928fcec75f4e
Talathon <kt.talatkhon@gmail.com>
2022-11-30 08:27:53 +0500

    tugadi

--- scpript.js
+++ scpript.js
@@ -1,29 +1,29 @@
-let SumOne = prompt("введите сумму+" , 0);
-let SumTwo = prompt("введите сумму+" , 0);
+let SumOne = prompt("плюс 1" , 0);
+let SumTwo = prompt("плюс 2" , 0);
 let Sum1 = parseInt(SumOne);
 let Sum2 = parseInt(SumTwo);
 let SumResult = Sum1 + Sum2;
 alert("Ответ:" + SumResult)
 
 
-let mOne = prompt("введите сумму-" , 0);
-let mTwo = prompt("введите сумму-" , 0);
+let mOne = prompt("минус 1" , 0);
+let mTwo = prompt("минус 2" , 0);
 let m1 = parseInt(mOne);
 let m2 = parseInt(mTwo);
 let mResult = m1 - m2;
 alert("Ответ:" + mResult)
 
 
-let dOne = prompt("введите сумму/" , 0);
-let dTwo = prompt("введите сумму/" , 0);
+let dOne = prompt("разделить 1" , 0);
+let dTwo = prompt("разделить 2" , 0);
 let d1 = parseInt(dOne);
 let d2 = parseInt(dTwo);
 let dResult = d1 / d2;
 alert("Ответ:" + dResult)
 
 
-let muOne = prompt("введите сумму*" , 0);
-let muTwo = prompt("введите сумму*" , 0);
+let muOne = prompt("умножить 1" , 0);
+let muTwo = prompt("умножить 2" , 0);
 let mu1 = parseInt(muOne);
 let mu2 = parseInt(muTwo);
 let muResult = mu1 * mu2;
